13cfccd748d5543c9bab9f86229f4640025b2962
Chris Philbin <chris@chrisphilbin.net>
2019-03-17 23:51:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    @item = params[:item]
+    erb :checkout
+  end
 end
